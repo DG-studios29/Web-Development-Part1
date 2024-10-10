f3854243c9f1e6377d6a21e7e60ec782fc991f1f
Dhillan Gopal <70883550+DG-studios29@users.noreply.github.com>
2024-10-10 19:33:53 +0200

    Task 2

---
+++ Task 2/Greeting Example/greeting.js
@@ -0,0 +1,7 @@
+// Code here for Compulsory Task 1
+
+let myName = prompt("What is your name?");
+let myAge = prompt("What is your age?");
+console.log("Hello " + myName + "!");
+console.log("You are "+myAge+" years of age.");
+console.log("\n It's nice to meet you!")
---
+++ Task 2/Shopping Example/shopping.js
@@ -0,0 +1,9 @@
+// Code here for Compulsory Task 2
+let item1 = prompt("Enter item 1:");
+let item2 = prompt("Enter item 2:");
+let item3 = prompt("Enter item 3:");
+
+console.log("My Shopping List:");
+console.log(item1);
+console.log(item2);
+console.log(item3);
